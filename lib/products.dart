d1ef683834d02e9e6feab62765df8fbe792c4080
Jeremy De La Cruz <jeremydelacruz219@gmail.com>
2019-04-30 00:13:36 -0400

    Implemented navigation via pages, side menus, modals, and alerts

--- lib/products.dart
+++ lib/products.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_course/pages/product.dart';
 
 class Products extends StatelessWidget {
   final List<Map<String, String>> products;
+  final Function deleteProduct;
 
-  Products([this.products = const []]) {
+  Products(this.products, {this.deleteProduct}) {
     print('[Products Widget] Constructor');
   }
 
@@ -19,15 +19,14 @@ class Products extends StatelessWidget {
             children: <Widget>[
               FlatButton(
                 child: Text('Details'),
-                onPressed: () => Navigator.push(
+                onPressed: () => Navigator.pushNamed<bool>(
                       context,
-                      MaterialPageRoute(
-                        builder: (BuildContext context) => ProductPage(
-                              products[index]['title'],
-                              products[index]['image'],
-                            ),
-                      ),
-                    ),
+                      '/product/$index',
+                    ).then((bool value) {
+                      if (value) {
+                        deleteProduct(index);
+                      }
+                    }),
               )
             ],
           )
